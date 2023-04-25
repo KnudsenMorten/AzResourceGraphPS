c8f6bcb2df0c804fbdb4f06589f0353652165cb5
Morten Knudsen <mok@mortenknudsen.net>
2023-04-25 23:52:39 +0200

    Update Demo.ps1

--- Demo.ps1
+++ Demo.ps1
@@ -18,74 +18,30 @@ Query-AzureResourceGraph -DataType $QueryParams[0] `
                          -Query $QueryParams[1] `
                          -Scope "MG" `
                          -ScopeTarget "mg-2linkit" `
-                         -IncludeScopeRoot $IncludeScopeRoot `
                          -First 5
                           
-########################
-# UseTenantScope
-########################
 
-Get-ARG-AzSubscriptions | Query-AzureResourceGraph -Scope "Tenant" -IncludeScopeRoot $IncludeScopeRoot
-                                                       
+# AzSubscriptions (Subscriptions)
+    KQL-ARG-AzSubscriptions | Query-AzureResourceGraph -Scope "Tenant" `
 
+    KQL-ARG-AzSubscriptions | Query-AzureResourceGraph -Scope "MG" `
+                                                       -ScopeTarget "2linkit" `
 
+# AzRoleAssignments (Role Assignments)
+    KQL-ARG-AzRoleAssignments | Query-AzureResourceGraph -Scope "MG" `
+                                                         -ScopeTarget "2linkit"
 
+# AzMGsWithParentHierarchy (Management Group)
+    KQL-ARG-AzMGsWithParentHierarchy | Query-AzureResourceGraph -Scope "MG" `
+                                                                -ScopeTarget "2linkit" `
+                                                                -Skip 3
 
-<#
+    KQL-ARG-AzMGsWithParentHierarchy | Query-AzureResourceGraph -Scope "MG" `
+                                                                -ScopeTarget "2linkit" `
+                                                                -First 3
 
-#####################################################################################################################
-# Getting Management groups - using Azure Resource Graph - limited to children under $ManagementGroupName
-#
-# Output: $AzMGs 
-# -------------------------------------------------------------------------------------------------------------------
-# Search-AzGraph will only include children objects under the specific management group; not the actual root management
-# group. But we need array to include root management group, when checking for orphaned security principels (task 1). 
-# Therefore we will build a new Array (AzMGsWithRoot)
-#
-# Output: $AzMGsWithRoot
-#####################################################################################################################
+    KQL-ARG-AzMGsWithParentHierarchy | Query-AzureResourceGraph -Scope "MG" `
+                                                                -ScopeTarget "2linkit"
 
-    Write-Output "Getting Management Groups from Azure Resource Graph (root: $($ManagementGroupName))"
-    $AzMGs = @()
-    $pageSize = 1000
-    $iteration = 0
-    $searchParams = @{
-					    Query = "resourcecontainers `
-                        | where type == 'microsoft.management/managementgroups' `
-                        | extend mgParent = properties.details.managementGroupAncestorsChain `
-                        | mv-expand with_itemindex=MGHierarchy mgParent `
-                        | project id, name, properties.displayName, mgParent, MGHierarchy, mgParent.name `
-                        | sort by MGHierarchy asc "
-					    First = $pageSize
- 			            }
+    KQL-ARG-AzMGsWithParentHierarchy | Query-AzureResourceGraph -Scope "Tenant"
 
-    $results = do {
-	    $iteration += 1
-	            $pageResults = Search-AzGraph  @searchParams -ManagementGroup $ManagementGroupName
-	    $searchParams.Skip += $pageResults.Count
-	    $AzMGs += $pageResults
-    } while ($pageResults.Count -eq $pageSize)
-
-    #------------------------------------------------------------------------------------------------------------------------------------
-    # Special for Task #1 - remove orphaned security principals
-    # Search-AzGraph will only include children objects under the specific management group - but when using data to
-    # remove orphaned objects, we also need to have the actual management group as part of the scope
-    # Therefore we will build a new Array $AzMGsWithRoot
-
-        $AzMGWithRoot = @()
-
-        # Getting the root variable
-        $AzMGWithRoot_Temp = New-Object PSObject
-        $AzMGWithRoot_Temp | Add-Member -MemberType NoteProperty -Name Id -Value ((Get-AzManagementGroup -GroupId $ManagementGroupName -WarningAction SilentlyContinue).id)
-        $AzMGWithRoot += $AzMGWithRoot_Temp
-
-        # Now we get the children from AzMGs
-        ForEach ($Obj in $AzMGs)
-            {
-                $AzMGWithRoot_Temp = New-Object PSObject
-                $AzMGWithRoot_Temp | Add-Member -MemberType NoteProperty -Name Id -Value $Obj.id
-                $AzMGWithRoot += $AzMGWithRoot_Temp
-            }
-
-    #------------------------------------------------------------------------------------------------------------------------------------
-#>
