dd3bdee86ee969536c821c112070ebce05027403
Morten Knudsen <mok@mortenknudsen.net>
2023-04-28 15:32:42 +0200

    Update Demo.ps1

--- Demo.ps1
+++ Demo.ps1
@@ -183,9 +183,33 @@ Write-Output ""
 # MAIN PROGRAM
 ############################################################################################################################################
 
-    #---------------------------------------------------------------------------------------------                          
-    # Azure Management Group - with parent/Hierarchy
-    #---------------------------------------------------------------------------------------------                          
+#---------------------------------------------------------------------------------------------                          
+# Azure Management Group - with parent/Hierarchy
+#---------------------------------------------------------------------------------------------                          
+
+$Query = @"
+    resourcecontainers 
+    | where type == 'microsoft.management/managementgroups' 
+    | extend mgParent = properties.details.managementGroupAncestorsChain 
+    | mv-expand with_itemindex=MGHierarchy mgParent 
+    | project id, name, properties.displayName, mgParent, MGHierarchy, mgParent.name 
+    | sort by MGHierarchy asc
+"@
+
+    $Query | Query-AzureResourceGraph -Scope "Tenant"
+
+$Query = @"
+    patchinstallationresources
+    | where type has "softwarepatches" and properties !has "version"
+    | extend machineName = tostring(split(id, "/", 8)), resourceType = tostring(split(type, "/", 0)), tostring(rgName = split(id, "/", 4)), tostring(RunID = split(id, "/", 10))
+    | extend prop = parse_json(properties)
+    | extend lTime = todatetime(prop.lastModifiedDateTime), patchName = tostring(prop.patchName), kbId = tostring(prop.kbId), installationState = tostring(prop.installationState), classifications = tostring(prop.classifications)
+    | where lTime > ago(7d)
+    | project lTime, RunID, machineName, rgName, resourceType, patchName, kbId, classifications, installationState
+    | sort by RunID
+"@
+
+    $Query | Query-AzureResourceGraph -Scope "Tenant"
 
         #---------------------------------------------------------------------------------------------                          
         # Show query only
