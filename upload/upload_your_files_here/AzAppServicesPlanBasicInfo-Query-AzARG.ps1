a76e4b75b68fdd4f9a45b18dd589a02b1064ebf3
Morten Knudsen <121301838+KnudsenMorten@users.noreply.github.com>
2023-08-08 18:15:08 +0200

    Update AzAppServicesPlanBasicInfo-Query-AzARG.ps1

--- upload/upload_your_files_here/AzAppServicesPlanBasicInfo-Query-AzARG.ps1
+++ upload/upload_your_files_here/AzAppServicesPlanBasicInfo-Query-AzARG.ps1
@@ -1,1 +1,31 @@
+Function AzAppServicesPlanBasicInfo-Query-AzARG
+{
+  [CmdletBinding()]
+  param(
 
+          [Parameter()]
+            [switch]$Details = $false
+       )
+
+$Query = @"
+resources
+| where type=~'Microsoft.Web/serverfarms' 
+| extend NumberOfApps = properties.numberOfSites
+| extend sku = sku.name
+| project Name=['name'], sku, NumberOfApps, Location=['location']
+"@
+
+$Description = "App Service Plans Basic Information"
+$Category    = "Configuration"
+$Credit      = "Wesley Haakman (@whaakman)"
+
+If ($Details)
+    {
+        Return $Query, $Description, $Category, $Credit
+    }
+Else
+    {
+        # only return Query
+        Return $Query
+    }
+}
